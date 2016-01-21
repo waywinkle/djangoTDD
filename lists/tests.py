e9887b9e4db6db98e7562614d7fe93578c67914d
waywinkle <jabmiddleton@gmail.com>
2016-01-21 19:47:03 +1300

    front page now generated from template

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import Client
 
 from lists.views import home_page
 
@@ -13,7 +14,23 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        request = Client()
+        response = request.get('/')
         expected_html = render_to_string('home.html')
+        self.assertTemplateUsed(response, 'home.html')
+        #self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+                {'new_item_text', 'A new list item'}
+        )
+        print(response.content.decode())
         self.assertEqual(response.content.decode(), expected_html)
