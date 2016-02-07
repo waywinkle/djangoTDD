cc4c1345e1f6c9c764ea4dd4c29ecb2b1420ad81
waywinkle <jabmiddleton@gmail.com>
2016-02-08 10:28:59 +1300

    changed out fts to be in seperate files.

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,27 @@
+from functional_tests.base import FunctionalTest
+
+
+class LayoutAndStylingTest(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # she notices that the input box is nicely centred
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # she starts a new list and finds the input is nicely centred there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
