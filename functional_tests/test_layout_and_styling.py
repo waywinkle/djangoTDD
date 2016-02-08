af881c82fc359c35fbdbdb4e633dc900513727ab
waywinkle <jabmiddleton@gmail.com>
2016-02-09 08:27:03 +1300

    rename all item input ids and name. everything still broken

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -10,7 +10,7 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.set_window_size(1024, 768)
 
         # she notices that the input box is nicely centred
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
@@ -19,7 +19,7 @@ class LayoutAndStylingTest(FunctionalTest):
 
         # she starts a new list and finds the input is nicely centred there too
         inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
--- lists/views.py
+++ lists/views.py
@@ -1,34 +1,17 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
+
 from lists.models import Item, List
 from lists.forms import ItemForm
-
-# Create your views here.
 
 
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    error = None
-
-    if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['item_text'], list=list_)
-            item.full_clean()
-            item.save()
-            return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
-
-    return render(request, 'list.html', {'list': list_, 'error': error})
-
-
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -37,4 +20,20 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
     return redirect(list_)
+
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    error = None
+
+    if request.method == 'POST':
+        try:
+            item = Item(text=request.POST['text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(list_)
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
