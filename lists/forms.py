af190c532837666ba07de56e7af89f388996fea6
waywinkle <jabmiddleton@gmail.com>
2016-02-17 20:34:23 +1300

    use form in all views, back to working state

--- lists/forms.py
+++ lists/forms.py
@@ -14,7 +14,7 @@ class ItemForm(forms.models.ModelForm):
             'text': forms.fields.TextInput(attrs={
                 'placeholder': 'Enter a to-do item',
                 'class': 'form-control input-lg',
-            })
+            }),
         }
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
